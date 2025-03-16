399645ffa4597e0363a663746ce93d44ee28a0cf
Vinicius Silva <viniciuspdasilva.dev@gmail.com>
2025-03-16 14:12:30 -0300

    Add classes

--- core/models/Engine.js
+++ core/models/Engine.js
@@ -1,1 +1,88 @@
-export class Engine {}
+import {Sala} from "./Sala";
+import {validate} from "bycontract";
+import {Mochila} from "./Mochila";
+
+export class Engine {
+    #mochila;
+    #salaCorrente;
+    #fim;
+
+    constructor(){
+        this.#mochila = new Mochila();
+        this.#salaCorrente = null;
+        this.#fim = false;
+        this.criaCenario();
+    }
+
+    get mochila(){
+        return this.#mochila;
+    }
+
+    get salaCorrente(){
+        return this.#salaCorrente;
+    }
+
+    set salaCorrente(sala){
+        validate(sala,Sala);
+        this.#salaCorrente = sala;
+    }
+
+    indicaFimDeJogo(){
+        this.#fim = true;
+    }
+
+    // Para criar um jogo deriva-se uma classe a partir de
+    // Engine e se sobrescreve o método "criaCenario"
+    criaCenario(){}
+
+    // Para poder acionar o método "joga" deve-se garantir que
+    // o método "criaCenario" foi acionado antes
+    joga() {
+        let novaSala = null;
+        let acao = "";
+        let tokens = null;
+        while (!this.#fim) {
+            console.log("-------------------------");
+            console.log(this.salaCorrente.textoDescricao());
+            acao = prompt("O que voce deseja fazer? ");
+            tokens = acao.split(" ");
+            switch (tokens[0]) {
+                case "fim":
+                    this.#fim = true;
+                    break;
+                case "pega":
+                    if (this.salaCorrente.pega(tokens[1])) {
+                        console.log("Ok! " + tokens[1] + " guardado!");
+                    } else {
+                        console.log("Objeto " + tokens[1] + " não encontrado.");
+                    }
+                    break;
+                case "inventario":
+                    console.log("Ferramentas disponiveis para serem usadas: " + this.#mochila.inventario());
+                    break;
+                case "usa":
+                    if (this.salaCorrente.usa(tokens[1],tokens[2])) {
+                        console.log("Feito !!");
+                        if (this.#fim == true){
+                            console.log("Parabens, voce venceu!");
+                        }
+                    } else {
+                        console.log("Não é possível usar " + tokens[1] + "sobre" + tokens[2] + " nesta sala");
+                    }
+                    break;
+                case "sai":
+                    novaSala = this.salaCorrente.sai(tokens[1]);
+                    if (novaSala == null) {
+                        console.log("Sala desconhecida ...");
+                    } else {
+                        this.#salaCorrente = novaSala;
+                    }
+                    break;
+                default:
+                    console.log("Comando desconhecido: " + tokens[0]);
+                    break;
+            }
+        }
+        console.log("Jogo encerrado!");
+    }
+}
--- core/models/Sala.js
+++ core/models/Sala.js
@@ -1,21 +1,11 @@
 import {validate} from "bycontract";
 import {Engine} from "./Engine";
-import {Ferramenta} from "./Ferramenta";
-import {Object} from "./Object"
 
 
 export class Sala {
-    /**
-     * @type {#name: String}
-     * */
+
     #name;
-    /**
-     * @type {#ferramentas: Array<Ferramenta>}
-     * */
     #ferramentas;
-    /**
-     * @type {#objects: Array<Object>}
-     * */
     #objects;
     #ports;
     #engine;
@@ -24,9 +14,9 @@ export class Sala {
         validate(arguments, ["String", Engine]);
         this.#name = name;
         this.#engine = engine;
-        this.#ferramentas = [];
-        this.#objects = [];
-        this.#ports = [];
+        this.#ferramentas = new Map();
+        this.#objects = new Map();
+        this.#ports = new Map();
     }
 
     get name() {
@@ -41,13 +31,59 @@ export class Sala {
         return this.#ferramentas;
     }
 
+    get ports() {
+        return this.#ports;
+    }
+
     get engine() {
         return this.#engine;
     }
 
     objetosDisponiveis() {
-        let arrObj = [...this.#objects];
+        let arrObj = [...this.#objects.values()];
         return arrObj.map(obj => obj.name + ":" + obj.description);
+    }
+
+    ferramentasDisponiveis() {
+        let arrObj = [...this.#ferramentas.values()];
+        return arrObj.map(f => f.name);
+    }
+
+    pega(nomeFerramenta) {
+        validate(nomeFerramenta, "String");
+        let ferramenta = this.#ferramentas.get(nomeFerramenta);
+        if (ferramenta != null) {
+            this.#engine.mochila.guarda(ferramenta);
+            this.#ferramentas.delete(ferramenta);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    sai(porta) {
+        validate(porta, "String");
+        return this.#ports.get(porta);
+    }
+
+    textoDescricao() {
+        let descricao = "Você está no "+this.nome+"\n";
+        if (this.objects.size === 0){
+            descricao += "Não há objetos na sala\n";
+        }else{
+            descricao += "Objetos: "+this.objetosDisponiveis()+"\n";
+        }
+        if (this.ferramentas.size === 0){
+            descricao += "Não há ferramentas na sala\n";
+        }else{
+            descricao += "Ferramentas: "+this.ferramentasDisponiveis()+"\n";
+        }
+        descricao += "Portas: "+this.portasDisponiveis()+"\n";
+        return descricao;
+    }
+
+    usa(ferramenta, objeto) {
+        return false;
     }
 
 }
